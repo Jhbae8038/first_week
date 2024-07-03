c9da90bbe23b0a1e2e09615cd3a67f05711ff612
“Junhyeong <jhbae03@kaist.ac.kr>
2024-07-03 17:18:14 +0900

    free13

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:kaist_summer_camp/screen/phonebook_screen.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:device_info_plus/device_info_plus.dart';
+import 'dart:async';
 
 
 
@@ -38,6 +39,68 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
           useMaterial3: true,
         ),
-        home: const HomeScreen());
+        home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+  bool _showFirstImage = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: Duration(seconds: 1),
+      vsync: this,
+    )..repeat(reverse: true);
+
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    );
+
+    // 3초 후 메인 화면으로 전환
+    Timer(Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(
+        builder: (context) => HomeScreen(),
+      ));
+    });
+
+    // 애니메이션으로 이미지 전환
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        _showFirstImage = !_showFirstImage;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: FadeTransition(
+          opacity: _animation,
+          child: _showFirstImage
+              ? Image.asset('asset/splash.jpg') // 첫 번째 스플래시 이미지 경로
+              : Image.asset('asset/splash2.jpg'), // 두 번째 스플래시 이미지 경로
+        ),
+      ),
+    );
   }
 }
